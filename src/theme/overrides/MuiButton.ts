df723b29d34eb22d302df4c6dd0b223e626a33a1
Darius <dariusz.sobkowicz@gmail.com>
2020-05-28 06:52:48 +0200

    button hover state

--- src/theme/overrides/MuiButton.ts
+++ src/theme/overrides/MuiButton.ts
@@ -1,6 +1,9 @@
 export default {
     root: {
-        backgroundColor: '#7dc0de',
-        borderRadius: '3px'
-    }
-}
+        backgroundColor: 'rgba(125, 192, 222, 1)',
+        borderRadius: '3px',
+        '&:hover': {
+            backgroundColor: 'rgba(125, 192, 222, 0.5)',
+        },
+    },
+};
