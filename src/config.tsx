7cfa13fecb6777520f8f4d96da8dd1156d9d9748
Darius <dariusz.sobkowicz@gmail.com>
2020-06-01 15:04:55 +0200

    config update

--- src/config.tsx
+++ src/config.tsx
@@ -1,8 +1,18 @@
 const config = {
-  clientId: '5ae9dfadad742331a2a0',
-  url: 'https://nc-dev1.cryptonomic-infra.tech',
-  toUrl: 'https://github.com/Cryptonomic/Deployments/raw/master/Terms_of_Service.pdf',
-  ppUrl: 'https://github.com/Cryptonomic/Deployments/raw/master/Privacy_Policy.pdf'
+    clientId: '5ae9dfadad742331a2a0',
+    url: 'https://nc-dev1.cryptonomic-infra.tech',
+    terms: 'https://github.com/Cryptonomic/deployments',
+    twitter: 'https://twitter.com/cryptonomictech',
+    gitHub: 'https://github.com/Cryptonomic',
+    riot:
+        'https://matrix.to/#/!rUwpbdwWhWgKINPyOD:cryptonomic.tech?via=cryptonomic.tech&via=matrix.org&via=ostez.com',
+    medium: 'https://medium.com/the-cryptonomic-aperiodical',
+    handbook: 'https://handbook.cryptonomic.tech/',
+    support: 'https://cryptonomic.tech/',
+    conseil: 'https://github.com/Cryptonomic/Conseil/wiki',
+    conseiljs:
+        'https://github.com/Cryptonomic/ConseilJS/wiki/Tutorial:-Querying-for-Tezos-alphanet-data-using-the-ConseilJS-v2-API',
+    userTools: 'https://cryptonomic.tech/usertools.html',
 };
 
 export default config;
